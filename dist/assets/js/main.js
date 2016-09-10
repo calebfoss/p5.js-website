350ac679a966a0aa965c61e70ce76137c61fa614
Lauren McCarthy <laurmccarthy@gmail.com>
2016-09-10 16:36:33 -0700

    adding tagline #19

--- dist/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  Prism.plugins.NormalizeWhitespace.setDefaults({
-    'remove-trailing': true,
-    'left-trim': true,
-    'right-trim': true,
-    'remove-indent': true
-  });
-});
