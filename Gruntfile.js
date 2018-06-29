e632c9ab3632f57f178e83240469190ffc4cae70
limzykenneth <limzy.kenneth@gmail.com>
2018-06-29 22:20:15 +0800

    Add grunt-contrib-requirejs to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,6 +319,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-file-append');
   grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // multi-tasks: collections of other tasks
   grunt.registerTask('server', [
